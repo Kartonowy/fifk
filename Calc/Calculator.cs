a0f4e50ab43f26ffc3b5c8df668187dbb0b28ece
Kartonowy <ttrollface.com.pl@gmail.com>
2024-10-20 22:26:23 +0200

    JSDoc

--- Calc/Calculator.cs
+++ Calc/Calculator.cs
@@ -9,6 +9,13 @@ namespace Calc
 {
     public class Calculator
     {
+        /// <summary>
+        /// Adds two numbers together
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>32-bit integer</returns>
+        /// <exception cref=""></exception>
         public static int Add(int x, int y)
         {
             return x + y;
